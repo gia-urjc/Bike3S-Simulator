3a8632cbadc5a1aa410741d3f90e710bd0f5c7f0
cruizba <charlesrb94@outlook.com>
2017-09-13 12:42:55 +0200

    Servidor configurado y funcionando

---
+++ backend-bikesurbanfloats/src/main/java/com/urjc/iagroup/bikesurbanfloats/Application.java
@@ -0,0 +1,18 @@
+package com.urjc.iagroup.bikesurbanfloats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableAutoConfiguration
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
