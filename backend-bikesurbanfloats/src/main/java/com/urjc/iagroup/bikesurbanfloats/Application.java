908323f5e8ba6d3a284feb328d698a3a2a6118b6
cruizba <charlesrb94@outlook.com>
2017-09-15 11:34:14 +0200

    Changed pom project

--- backend-bikesurbanfloats/src/main/java/com/urjc/iagroup/bikesurbanfloats/Application.java
+++ backend-bikesurbanfloats/src/main/java/com/urjc/iagroup/bikesurbanfloats/Application.java
@@ -1,18 +1,13 @@
 package com.urjc.iagroup.bikesurbanfloats;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 /**
  * Hello world!
  *
  */
-@SpringBootApplication
-@EnableAutoConfiguration
+
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        System.out.println("Hola mundo");
     }
 }
