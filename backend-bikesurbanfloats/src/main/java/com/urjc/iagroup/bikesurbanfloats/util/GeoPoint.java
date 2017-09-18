aaa5d470762678412f59bb660b4332807c2422d2
Tao Cumplido <taocumplido@gmail.com>
2017-09-18 16:04:17 +0200

    GeoPoint: changed distance calculation from vector to haversine

--- backend-bikesurbanfloats/src/main/java/com/urjc/iagroup/bikesurbanfloats/util/GeoPoint.java
+++ backend-bikesurbanfloats/src/main/java/com/urjc/iagroup/bikesurbanfloats/util/GeoPoint.java
@@ -2,6 +2,11 @@ package com.urjc.iagroup.bikesurbanfloats.util;
 
 public class GeoPoint {
 
+    /**
+     * Earth radius in meters
+     */
+    private final static Double EARTH_RADIUS = 6371e3;
+
     private Double latitude;
     private Double longitude;
 
@@ -30,8 +35,19 @@ public class GeoPoint {
         this.longitude = longitude;
     }
 
+    /**
+     * Calculates the distance to another point using the haversine formula.
+     * @return The distance in meters.
+     * @see <a href="https://en.wikipedia.org/wiki/Haversine_formula">Haversine formula on Wikipedia</a>
+     */
     public Double distanceTo(GeoPoint point) {
-        GeoPoint delta = new GeoPoint(point.latitude - this.latitude, point.longitude - this.longitude);
-        return Math.sqrt(Math.pow(delta.latitude, 2) + Math.pow(delta.longitude, 2));
+        Double[] f = {Math.toRadians(this.latitude), Math.toRadians(point.latitude)};
+        Double[] l = {Math.toRadians(this.longitude), Math.toRadians(point.longitude)};
+        Double h = haversine(f[1] - f[0]) + Math.cos(f[0]) * Math.cos(f[1]) * haversine(l[1] - l[0]);
+        return 2 * EARTH_RADIUS * Math.asin(Math.sqrt(h));
+    }
+
+    private Double haversine(Double value) {
+        return Math.pow(Math.sin(value / 2), 2);
     }
 }
