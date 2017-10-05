106e2656e47745409cbac20aecf104cc25d88a0f
stimonm <s.timonm@alumnos.urjc.es>
2017-10-05 11:47:32 +0200

    set destinationStation and set position have been controlled in each event

--- backend-bikesurbanfloats/src/main/java/com/urjc/iagroup/bikesurbanfloats/events/EventSlotReservationTimeout.java
+++ backend-bikesurbanfloats/src/main/java/com/urjc/iagroup/bikesurbanfloats/events/EventSlotReservationTimeout.java
@@ -28,12 +28,14 @@ public class EventSlotReservationTimeout extends Event {
 
         Station destination = user.determineStation();
         int arrivalTime = user.timeToReach(destination.getPosition());
+        user.setDestinationStation(destination);
 
         if (user.decidesToReserveSlot(destination) && SystemInfo.reservationTime < arrivalTime) {
         				user.updatePosition(SystemInfo.reservationTime);
             user.cancelsSlotReservation(destination);
             newEvents.add(new EventSlotReservationTimeout(getInstant() + SystemInfo.reservationTime, user));
         } else {
+												user.setPosition(destination.getPosition());
             newEvents.add(new EventUserArrivesAtStationToReturnBike(getInstant() + arrivalTime, user, destination));
         }
 
