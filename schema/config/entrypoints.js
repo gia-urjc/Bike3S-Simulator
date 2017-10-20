34d722bb188b6d7a59f00722fd74b5da7978ac0f
Tao Cumplido <taocumplido@gmail.com>
2017-10-20 11:12:57 +0200

    fix entrypoint schema

--- schema/config/entrypoints.js
+++ schema/config/entrypoints.js
@@ -1,56 +1,66 @@
+const merge = require('../util/merge');
+
 const GeoPoint = require('../common/geopoint');
+
+const distributions = [
+    {
+        properties: {
+            type: { const: 'random' }
+        }
+    },
+    {
+        required: ['lambda'],
+        properties: {
+            type: { const: 'poisson' },
+            lambda: { type: 'number' }
+        },
+    }
+];
+
+const validItems = [
+    {
+        required: ['timeInstant'],
+        properties: {
+            timeInstant: {
+                type: 'integer',
+                minimum: 0
+            }
+        },
+    },
+    {
+        required: ['distribution'],
+        properties: {
+            distribution: {
+                oneOf: distributions.map((distribution) => merge(distribution, {
+                    type: 'object',
+                    additionalProperties: false,
+                    required: ['type']
+                })),
+            }
+        }
+    }
+];
 
 module.exports = {
     $schema: 'http://json-schema.org/draft-06/schema#',
     type: 'array',
     items: {
-        type: 'object',
-        required: ['userType', 'position'],
-        additionalProperties: false,
-        properties: {
-            userType: {
-                enum: [
-                    'UserTest',
-                ]
-            },
-            position: GeoPoint,
-            radio: {
-                type: 'integer',
-                minimum: 0,
-                required: false
-            }
-        },
-        oneOf: [
-            {
-                required: ['timeInstant'],
-                properties: {
-                    timeInstant: {
-                        type: 'integer',
-                        minimum: 0
-                    }
+        oneOf: validItems.map((item) => merge(item, {
+            type: 'object',
+            additionalProperties: false,
+            required: ['userType', 'position'],
+            properties: {
+                userType: {
+                    enum: [
+                        'UserTest',
+                    ]
                 },
-            },
-            {
-                distribution: {
-                    type: 'object',
-                    required: ['type'],
-                    additionalProperties: false,
-                    oneOf: [
-                        {
-                            properties: {
-                                type: { const: 'random' }
-                            }
-                        },
-                        {
-                            properties: {
-                                type: { const: 'poisson' },
-                                lambda: { type: 'number' }
-                            },
-                            required: ['lambda']
-                        }
-                    ],
+                position: GeoPoint,
+                radio: {
+                    type: 'integer',
+                    minimum: 0
                 }
-            }
-        ],
+            },
+        }))
     }
 };
