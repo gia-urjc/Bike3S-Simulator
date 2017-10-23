5834060d8a8d9a2b484a36f6eed44797444ee3c5
Tao Cumplido <taocumplido@gmail.com>
2017-10-23 21:33:42 +0200

    add reservation model

--- backend-bikesurbanfloats/src/main/java/com/urjc/iagroup/bikesurbanfloats/entities/Reservation.java
+++ backend-bikesurbanfloats/src/main/java/com/urjc/iagroup/bikesurbanfloats/entities/Reservation.java
@@ -1,75 +1,87 @@
 package com.urjc.iagroup.bikesurbanfloats.entities;
 
-public class Reservation {
-	
-	public enum ReservationType {
-		SLOT, BIKE
-	}
+import com.urjc.iagroup.bikesurbanfloats.entities.models.ReservationModel;
+import com.urjc.iagroup.bikesurbanfloats.util.IdGenerator;
 
-	public enum ReservationState {
-		FAILED, ACTIVE, EXPIRED, SUCCESSFUL
-	}
-	
-	public static int timeout;	
-	private int startInstant;  // instant when user makes the reservation
-	private int endInstant;  // instant when reservation is resolved or expired
-	private ReservationType type;
-	private ReservationState state;
-	private User user; 
-	private Station station;
-	private Bike bike;  // bike which user has reserved or wants to return
-	
-	public Reservation(int startInstant, ReservationType type, User user, Station station, Bike bike) {
-		this.startInstant = startInstant;
-		this.endInstant = -1; // reservation has'nt ended
-		this.type = type;
-		this.state = ReservationState.ACTIVE;
-		this.user = user;
-		this.station = station;
-		this.bike = bike;
-			}
-	
-	public Reservation(int startInstant, ReservationType type, User user, Station station) {
-		this.startInstant = startInstant;
-		this.endInstant = -1; // reservation has'nt ended
-		this.type = type;
-		this.state = ReservationState.FAILED;
-		this.user = user;
-		this.station = station;
-		this.bike = null;
-			}
-	
-	public int getStartInstant() {
-		return startInstant;
-	}
-	
-	public ReservationType getType() {
-		return type;
-	}
-	
-	public ReservationState getState() {
-		return state;
-	}
-	
-	public User getUser() {
-		return user;
-	}
-	
-	public Station getStation() {
-		return station;
-	}
-	
-	public Bike getBike() {
-		return bike;
-	}
+public class Reservation implements Entity, ReservationModel<User, Bike, Station> {
 
-public void expire() {
-	this.state = ReservationState.EXPIRED;
-	this.endInstant = this.startInstant + timeout;
+    public enum ReservationType {
+        SLOT, BIKE
+    }
+
+    public enum ReservationState {
+        FAILED, ACTIVE, EXPIRED, SUCCESSFUL
+    }
+
+    public static int VALID_TIME;
+
+    private static IdGenerator idGenerator = new IdGenerator();
+
+    private int id;
+    private int startInstant;  // instant when user makes the reservation
+    private int endInstant;  // instant when reservation is resolved or expired
+    private ReservationType type;
+    private ReservationState state;
+    private User user;
+    private Station station;
+    private Bike bike;  // bike which user has reserved or wants to return
+
+    public Reservation(int startInstant, ReservationType type, User user, Station station, Bike bike) {
+        this.id = idGenerator.next();
+        this.startInstant = startInstant;
+        this.endInstant = -1; // reservation has'nt ended
+        this.type = type;
+        this.state = ReservationState.ACTIVE;
+        this.user = user;
+        this.station = station;
+        this.bike = bike;
+    }
+
+    public Reservation(int startInstant, ReservationType type, User user, Station station) {
+        this(startInstant, type, user, station, null);
+        this.endInstant = startInstant;
+        this.state = ReservationState.FAILED;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getStartInstant() {
+        return startInstant;
+    }
+
+    public int getEndInstant() {
+        return endInstant;
+    }
+
+    public ReservationType getType() {
+        return type;
+    }
+
+    public ReservationState getState() {
+        return state;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Station getStation() {
+        return station;
+    }
+
+    public Bike getBike() {
+        return bike;
+    }
+
+    public void expire() {
+        this.state = ReservationState.EXPIRED;
+        this.endInstant = this.startInstant + VALID_TIME;
+    }
+
+    public void resolve(int endInstant) {
+        this.state = ReservationState.SUCCESSFUL;
+        this.endInstant = endInstant;
+    }
 }
-
-public void resolve(int endInstant) {
-	this.state = ReservationState.SUCCESSFUL;
-	this.endInstant = endInstant;
-}
-	}
