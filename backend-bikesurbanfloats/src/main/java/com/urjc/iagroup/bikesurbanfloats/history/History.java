8bc1d4343704066e0fe5b7ec9a21e18ca27d8568
stimonm <s.timonm@alumnos.urjc.es>
2017-11-20 14:30:02 +0100

    all methods of History class, ecept serializeChanges method, have been documented

--- backend-bikesurbanfloats/src/main/java/com/urjc/iagroup/bikesurbanfloats/history/History.java
+++ backend-bikesurbanfloats/src/main/java/com/urjc/iagroup/bikesurbanfloats/history/History.java
@@ -15,6 +15,11 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 
+/**
+ * This class finds out the changes which have happened through the entire simulation and registers them.  
+ * @author IAgroup
+ *
+ */
 public class History {
 
     private static Gson gson = new GsonBuilder()
@@ -23,10 +28,21 @@ public class History {
             .setFieldNamingPolicy(FieldNamingPolicy.IDENTITY)
             .setPrettyPrinting()
             .create();
-
+    
+    /**
+     * It is the initial states of the entities in the system.
+     */
     private static EntityCollection initialEntities = new EntityCollection();
+    
+    /**
+     * It is the current state of the entities in the system.
+     */
     private static EntityCollection updatedEntities = new EntityCollection();
-
+    
+   /**
+    * This map stores, for each momento of the simulation, the changes of the entities of 
+    * all the events ocurred in a specific time instant.  
+    */
     private static TreeMap<Integer, List<EventEntry>> serializedEvents = new TreeMap<>();
 
     private static Path outputPath = Paths.get("history");
@@ -37,11 +53,19 @@ public class History {
 
         // TODO: save history output path in the indicated path in configuration file
     }
-
+    
+    /**
+     * It saves in a file the initial states of all entities in the system and, in other,
+     * the changes that the entities have been suffering throught the entire simulation.
+     */
     public static void close() throws IOException {
 
         // TODO: maybe split entities.json into multiple files, e.g. entities/users.json
-
+    	
+    	/*
+    	 * It is a map with the names of the entities'  history classes as the key and
+    	 * a list of history classes of a concrete entity as the vallue.
+    	 */
         Map<String, Collection<HistoricEntity>> entries = new HashMap<>();
         initialEntities.getEntityMaps().forEach((historicClass, entities) -> {
             String jsonIdentifier = getJsonIdentifier(historicClass);
@@ -53,6 +77,11 @@ public class History {
 
         // TODO: copy configuration file
     }
+    
+    /**
+     * It registers a new entity in the map of initial entities and in the map of updated entities.
+     * @param entity It is the entity to register.
+     */
 
     public static void registerEntity(Entity entity) {
         Class<? extends HistoricEntity> historicClass = getReferenceClass(entity.getClass());
@@ -60,17 +89,35 @@ public class History {
         initialEntities.addToMapFor(historicClass, historicEntity);
         updatedEntities.addToMapFor(historicClass, historicEntity);
     }
-
+    
+    /**
+     * It creates an event entry (in the map of serialized events) to register all the 
+     * changes detected in the entities involved in the event and creates and saves 
+     * the corresponding created historical entities into the entity collection of updated entities. 
+     * @param event It is the event to register.
+     */
     public static void registerEvent(Event event) throws IOException {
+        
+        /* It creates the historical entities instances coreesponding to all the 
+         * entities involved in an evet. 
+         */
         List<HistoricEntity> historicEntities = new ArrayList<>();
-
         for (Entity entity : event.getEntities()) {
             historicEntities.add(instantiateHistoric(entity));
         }
-
+        
+        /* It obtains the changes that the created historical entities have suffered 
+         * with respect to previous ones. 
+         */
         Map<String, List<JsonObject>> changes = serializeChanges(historicEntities);
-
+        
+        /* If the map of serialized changes doesn't conatin an entry with the time 
+         * instance of this event, it creates it.
+         */
         if (!serializedEvents.containsKey(event.getInstant())) {
+        	/* A file which contains serialized events can only save 100 time instants
+        	 * So, if the map of serialized events already contains 100 entries, it muts be written in a file  
+        	 */
             if (serializedEvents.size() == 100) {
                 writeTimeEntries();
                 serializedEvents.clear();
@@ -78,29 +125,50 @@ public class History {
 
             serializedEvents.put(event.getInstant(), new ArrayList<>());
         }
-
+        
+        // It creates a new event entry and adds it to the map of serialized events
         serializedEvents.get(event.getInstant()).add(new EventEntry(event.getClass().getSimpleName(), changes));
-
+        
+        /* It adds all the historical entities created from the event to the entity 
+         * collection of updated entities 
+         */
         for (HistoricEntity entity : historicEntities) {
             updatedEntities.addToMapFor(entity.getClass(), entity);
         }
     }
-
+    
+    /**
+     * It creates a file and writes the specified information inside it.
+     * @param name It is the name of the file which is created.
+     * @param content It is the information which is written in the file. 
+     */
     private static void writeJson(String name, Object content) throws IOException {
+    	// it creates a file with the specified name in the history directory
         File json = outputPath.resolve(name).toFile();
         json.getParentFile().mkdirs();
+        
+        // it writes the specified content in the created file
         try (FileWriter writer = new FileWriter(json)) {
             gson.toJson(content, writer);
         }
     }
-
+    
+    /**
+     * It transforms the map of serialized events into a list of time entries and writes it 
+     * into a file whose name is created with a format which follows a concrete pattern. 
+     */
     private static void writeTimeEntries() throws IOException {
         List<TimeEntry> timeEntries = new ArrayList<>();
 
         serializedEvents.forEach((time, eventEntries) -> {
             timeEntries.add(new TimeEntry(time, eventEntries));
         });
-
+        
+        /*
+         * The file name is created with the following format: 
+         * "first time instant of the simulation-last time instant of the simulation.number of
+         * registered time instants of the simulation.json"
+         */
         String fileName = new StringBuilder()
                 .append(serializedEvents.firstKey()).append("-")
                 .append(serializedEvents.lastKey()).append("_")
@@ -108,7 +176,12 @@ public class History {
 
         writeJson(fileName, timeEntries);
     }
-
+    
+    /**
+     * FALTA: 
+     * @param entities
+     * @return
+     */
     private static Map<String, List<JsonObject>> serializeChanges(List<HistoricEntity> entities) {
         Map<String, List<JsonObject>> changes = new HashMap<>();
 
@@ -152,7 +225,7 @@ public class History {
     }
 
     /**
-     * It finds out, from an entity class, the corresponding history class.
+     * It finds out, from an entity class, the corresponding historical class.
      * @param entityClass It is the entity class whose corresponding history class musts be found out.
      * @return the corresponding history class to the entity class.
      */
@@ -169,7 +242,12 @@ public class History {
 
         return referenceClasses[0].value();
     }
-
+    
+    /**
+     * It obtains the Json identifier string of a specific historical class.
+     * @param historicClass It is the history class whose identifier wants to be found out.
+     * @return the string of a Json identifier corresponding to the specified histry class. 
+     */
     private static String getJsonIdentifier(Class<? extends HistoricEntity> historicClass) {
         JsonIdentifier[] jsonIdentifiers = historicClass.getAnnotationsByType(JsonIdentifier.class);
 
@@ -185,12 +263,13 @@ public class History {
     }
 
     /**
-     * It creates, from a specific entity, the corresponding concrete history. 
+     * It creates, from a specific entity, the instance of the corresponding historical class. 
      * @param entity It is the entity whose history must be created.
      * @return the concrete history corresponding to the entity.
      */
     @SuppressWarnings("unchecked")
     private static HistoricEntity instantiateHistoric(Entity entity) {
+
         Class<? extends Entity> entityClass = entity.getClass();
         Class<? extends HistoricEntity> historicClass = getReferenceClass(entityClass);
 
@@ -211,7 +290,13 @@ public class History {
             throw new IllegalStateException("Error trying to instantiate " + historicClass);
         }
     }
-
+    
+    /**
+     * It creates a Json object which contains the previous and current states of specific property..
+     * @param oldProperty It is the previous state of the property.
+     * @param newProperty It is the current state of the property.
+     * @return a Json object which conatins the previous and curent states of the property.
+     */
     private static JsonObject propertyChange(Object oldProperty, Object newProperty) {
         if (Objects.equals(oldProperty, newProperty)) return null;
 
@@ -221,6 +306,7 @@ public class History {
 
         return property;
     }
+    
     
     /**
      * This class is used to save the histories of all the entities of the system.
--- backend-bikesurbanfloats/src/main/java/src/main/util/index.ts
+++
@@ -1,1 +0,0 @@
-export { HistoryReader } from './historyreader';
