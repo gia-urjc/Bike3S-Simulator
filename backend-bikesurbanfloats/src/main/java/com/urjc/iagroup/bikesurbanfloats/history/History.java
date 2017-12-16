b44d0b3a6f68a8327583befbe871df4b8d9dcab0
Tao Cumplido <taocumplido@gmail.com>
2017-12-16 20:05:10 +0100

    backend: implement equals & hashCode for history id reference

--- backend-bikesurbanfloats/src/main/java/com/urjc/iagroup/bikesurbanfloats/history/History.java
+++ backend-bikesurbanfloats/src/main/java/com/urjc/iagroup/bikesurbanfloats/history/History.java
@@ -432,6 +432,20 @@ public class History {
         public IdReference(Class<? extends HistoricEntity> type, List<Integer> idList) {
             this(type, (Object)idList);
         }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            IdReference that = (IdReference) o;
+            return Objects.equals(type, that.type) &&
+                    Objects.equals(id, that.id);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(type, id);
+        }
     }
 
 }
