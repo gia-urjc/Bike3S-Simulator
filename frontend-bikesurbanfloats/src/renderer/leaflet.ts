76bc76fde00ada665e6b10613a0a51468ebf29f3
Tao Cumplido <taocumplido@gmail.com>
2017-12-18 16:44:04 +0100

    frontend: fix leaflet default icon

---
+++ frontend-bikesurbanfloats/src/renderer/leaflet.ts
@@ -0,0 +1,13 @@
+import 'leaflet/dist/leaflet.css';
+
+import { icon, Marker } from 'leaflet';
+
+Marker.prototype.options.icon = icon({
+    iconUrl: 'assets/leaflet-default-marker-icon.png',
+    shadowUrl: 'assets/leaflet-default-marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    tooltipAnchor: [16, -28],
+    shadowSize: [41, 41]
+});
