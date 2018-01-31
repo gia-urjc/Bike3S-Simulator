ffdba152c8c28216aa4ff370f23e900a6f7a2c40
stimonm <s.timonm@alumnos.urjc.es>
2018-01-31 16:17:46 +0100

    some little changes

--- frontend-bikesurbanfleets/src/main/dataAnalysis/systemDataTypes/Entities.ts
+++ frontend-bikesurbanfleets/src/main/dataAnalysis/systemDataTypes/Entities.ts
@@ -5,22 +5,11 @@ export interface Entity extends  PlainObject {
 }
 
 export interface User extends Entity {
-    
+   
 }
 
 export interface Station extends Entity {
     capacity: number;
-    reservedBikes: number;
-    reservedSlots: number;
-    
-}
-
-export enum ReservationType {
-    BIKE, SLOT
-}
-
-export enum ReservationState {
-    FAILED, SUCCESSFUL
 }
 
 export interface Reservation extends Entity {
