47233dfa06eaec6278321fd932053389e521fec8
stimonm <s.timonm@alumnos.urjc.es>
2018-02-09 12:37:22 +0100

    system global values created

---
+++ frontend-bikesurbanfleets/src/main/dataAnalysis/analysis/SystemGlobalValues.ts
@@ -0,0 +1,15 @@
+export class SystemGlobalValues {
+  private int totalRentals; 
+  private int totalBikeReservations;
+  private int totalSlotReservations;
+  
+  private int successfulRentals;
+  private int successfulBikeReservations;
+  private int successfulSlotReservations;  private int failedRentals;
+  
+  private int failedBikeReservations;
+  private int failedSlotReservations;
+  private double demandSatisfaction;
+  
+   
+}
