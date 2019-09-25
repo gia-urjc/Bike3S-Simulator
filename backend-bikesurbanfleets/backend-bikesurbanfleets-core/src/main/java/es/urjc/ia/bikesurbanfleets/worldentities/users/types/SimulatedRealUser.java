4869446d1db91bf8cccfeb6af61e6cc0b0f8a29f
holger <holger@socrates.escet.urjc.es>
2019-09-25 17:50:00 +0200

    small changes

---
+++ backend-bikesurbanfleets/backend-bikesurbanfleets-core/src/main/java/es/urjc/ia/bikesurbanfleets/worldentities/users/types/SimulatedRealUser.java
@@ -0,0 +1,134 @@
+package es.urjc.ia.bikesurbanfleets.worldentities.users.types;
+
+import com.google.gson.JsonObject;
+import es.urjc.ia.bikesurbanfleets.common.graphs.GeoPoint;
+import static es.urjc.ia.bikesurbanfleets.common.util.ParameterReader.getParameters;
+import es.urjc.ia.bikesurbanfleets.services.SimulationServices;
+import es.urjc.ia.bikesurbanfleets.worldentities.stations.entities.Station;
+import es.urjc.ia.bikesurbanfleets.worldentities.users.User;
+import es.urjc.ia.bikesurbanfleets.worldentities.users.UserDecision;
+import es.urjc.ia.bikesurbanfleets.worldentities.users.UserDecisionGoToPointInCity;
+import es.urjc.ia.bikesurbanfleets.worldentities.users.UserDecisionLeaveSystem;
+import es.urjc.ia.bikesurbanfleets.worldentities.users.UserDecisionStation;
+import es.urjc.ia.bikesurbanfleets.worldentities.users.UserParameters;
+import es.urjc.ia.bikesurbanfleets.worldentities.users.UserType;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author holger
+ */
+@UserType("USER_SIMULATED")
+public class SimulatedRealUser extends User {
+
+    @Override
+    public UserDecision decideAfterAppearning() {
+        Station s = determineStationToRentBike();
+        if (s != null) { //user has found a station
+            return new UserDecisionStation(s, false);
+        } //if not he would leave
+        return new UserDecisionLeaveSystem();
+    }
+
+    @Override
+    public UserDecision decideAfterFailedRental() {
+        return new UserDecisionLeaveSystem();
+    }
+
+    //no reservations will take place
+    @Override
+    public UserDecision decideAfterFailedBikeReservation() {
+        return null;
+    }
+
+    @Override
+    public UserDecision decideAfterBikeReservationTimeout() {
+        return null;
+    }
+
+    @Override
+    public UserDecision decideAfterGettingBike() {
+        if (parameters.intermediatePosition != null) {
+            return new UserDecisionGoToPointInCity(parameters.intermediatePosition);
+        }else {
+            Station s = determineStationToReturnBike();
+            return new UserDecisionStation(s, false);
+        }
+    }
+
+    @Override
+    public UserDecisionStation decideAfterFailedReturn() {
+        Station s = determineStationToReturnBike();
+        return new UserDecisionStation(s, false);
+    }
+
+    @Override
+    public UserDecisionStation decideAfterFinishingRide() {
+        Station s = determineStationToReturnBike();
+        return new UserDecisionStation(s, false);
+    }
+
+    @Override
+    public UserDecisionStation decideAfterFailedSlotReservation() {
+        return null;
+    }
+
+    //TODO: should this method appear in User class?  
+    @Override
+    public UserDecisionStation decideAfterSlotReservationTimeout() {
+        return null;
+    }
+
+    @UserParameters
+    public class Parameters {
+
+        //default constructor used if no parameters are specified
+        private Parameters() {
+         }
+        GeoPoint intermediatePosition = null;
+
+    }
+
+    Parameters parameters;
+
+    public SimulatedRealUser(JsonObject userdef, SimulationServices services, long seed) throws Exception {
+        super(services, userdef, seed);
+        //***********Parameter treatment*****************************
+        //if this user has parameters this is the right declaration
+        //if no parameters are used this code just has to be commented
+        //"getparameters" is defined in USER such that a value of Parameters 
+        // is overwritten if there is a values specified in the jason description of the user
+        // if no value is specified in jason, then the orriginal value of that field is mantained
+        // that means that teh paramerts are all optional
+        // if you want another behaviour, then you should overwrite getParameters in this calss
+        this.parameters = new Parameters();
+        getParameters(userdef.getAsJsonObject("userType"), this.parameters);
+    }
+
+    @Override
+    protected Station determineStationToRentBike() {
+
+        Station destination = null;
+        List<Station> finalStations = informationSystem.getAllStationsOrderedByDistance(this.getPosition()).stream().collect(Collectors.toList());
+
+        if (!finalStations.isEmpty()) {
+            destination = finalStations.get(0);
+        }
+        return destination;
+    }
+
+    @Override
+    protected Station determineStationToReturnBike() {
+        Station destination = null;
+        List<Station> triedStations = getMemory().getStationsWithReturnFailedAttempts();
+        List<Station> finalStations = informationSystem.getAllStationsOrderedByDistance(this.destinationPlace);
+        finalStations.removeAll(triedStations);
+        if (!finalStations.isEmpty()) {
+            destination = finalStations.get(0);
+        } else {
+            throw new RuntimeException("[Error] User " + this.getId() + " cant return a bike, no slots");
+        }
+        return destination;
+    }
+}
