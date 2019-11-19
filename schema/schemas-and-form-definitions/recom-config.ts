6768645e60168b12c52eb9aba39a5aa15223d86f
albertofernandezurjc <alberto.fernandez@urjc.es>
2019-11-19 12:38:22 +0100

    Update list of recommenders

--- schema/schemas-and-form-definitions/recom-config.ts
+++ schema/schemas-and-form-definitions/recom-config.ts
@@ -3,27 +3,6 @@ import { sNumber } from "json-schema-builder-ts/dist/types";
 
 
 export const typeParameters = {
-    FULLCAPRECOMENDER: {
-        MAXDIFF: sNumber(),
-        MAXTOTALDIST: sNumber()
-    },
-    HOLGER_FixedDistanceRECOMENDER: {
-        MAXDIFF: sNumber(),
-        MAXTOTALDIST: sNumber()
-    },
-    HOLGERRECOMENDER: {
-        MAXDIFF: sNumber(),
-        MAXTOTALDIST: sNumber()
-    },
-    HOLGERRECOMENDER_RANDOM: {
-        MAXDIFF: sNumber(),
-        MAXTOTALDIST: sNumber()
-    },
-    HOLGERRECOMENDER_SURROUNDING: {
-        MAXDIFF: sNumber(),
-        MAXTOTALDIST: sNumber(),
-        MaxDistanceSurroundingStations: sNumber()
-    },
     AVAILABLE_RESOURCES: {
         maxDistanceRecommendation: UInt
     },
@@ -36,15 +15,6 @@ export const typeParameters = {
     RESOURCES_RATIO: {
         maxDistanceRecommendation: UInt
     },
-    SURROUNDING_STATIONS: {
-        maxDistanceRecommendation: UInt
-    },
-    SURROUNDING_STATIONS_COMPLEX_INCENTIVES: {
-        maxDistanceRecommendation: UInt
-    },
-    SURROUNDING_STATIONS_SIMPLE_INCENTIVES: {
-        maxDistanceRecommendation: UInt,
-    },
     SURROUNDING_AVAILABLE_RESOURCES: {
         maxDistanceRecommendation: UInt,
         MaxDistanceSurroundingStations: UInt
@@ -52,5 +22,40 @@ export const typeParameters = {
     SURROUNDING_DISTANCE_RESOURCES: {
         maxDistanceRecommendation: UInt,
         MaxDistanceSurroundingStations: UInt
+    },
+
+    DEMAND_PROBABILITY: {
+        maxDistanceRecommendation: UInt,
+        upperProbabilityBound: UInt,
+        desireableProbability: sNumber(),
+        desireableProbability: sNumber()
+    },
+    DEMAND_cost: {
+        maxDistanceRecommendation: UInt,
+        minimumMarginProbability: sNumber(),
+        minProbBestNeighbourRecommendation: sNumber(),
+        desireableProbability: sNumber(),
+        penalisationfactorrent: sNumber(),
+        penalisationfactorreturn: sNumber(),
+        maxStationsToReccomend: UInt,
+        unsucesscostRent: sNumber(),
+        unsucesscostReturn: sNumber(),
+        MaxCostValue: sNumber()
+    },
+        DEMAND_cost_prediction: {
+        maxDistanceRecommendation: UInt,
+        minimumMarginProbability: sNumber(),
+        minProbBestNeighbourRecommendation: sNumber(),
+        desireableProbability: sNumber(),
+        penalisationfactorrent: sNumber(),
+        penalisationfactorreturn: sNumber(),
+        maxStationsToReccomend: UInt,
+        unsucesscostRent: sNumber(),
+        unsucesscostReturn: sNumber(),
+        MaxCostValue: sNumber(),
+        PredictionNorm: UInt,
+        predictionWindow: UInt,
+        normmultiplier: sNumber()
     }
+
 };
