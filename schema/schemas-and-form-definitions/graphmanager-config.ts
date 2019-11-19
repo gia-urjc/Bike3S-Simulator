47239dfe1714cebd08ab12d96910f10062c04c23
albertofernandezurjc <alberto.fernandez@urjc.es>
2019-11-19 12:42:11 +0100

    Delete graphmanager-config.ts

--- schema/schemas-and-form-definitions/graphmanager-config.ts
+++
@@ -1,9 +0,0 @@
-import {sString} from "json-schema-builder-ts/dist/types";
-
-
-export const typeParameters = {
-    GRAPH_HOPPER: {
-        mapFile: sString,
-        tempDirectory: sString
-    }
-};
